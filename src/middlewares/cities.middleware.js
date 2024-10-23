b8f77444b5eff86633ef0f6bfefd29da790207d9
Danip25 <danielsambonic@hotmail.com>
2024-10-22 20:54:53 -0500

    feat: add validate fields middleware when load multiple cities

--- src/middlewares/cities.middleware.js
+++ src/middlewares/cities.middleware.js
@@ -5,3 +5,11 @@ export const NotHasAllRequiredFields = (req, res, next) => {
     }
     next();
 };
+export const CitiesLoadNotHaveAllFields = (req, res, next) => {
+    const { cities } = req.body;
+    if (cities && Array.isArray(cities) && cities?.length > 0) {
+        const hasCorrectMode = cities.some(({ city, country, place, place_to_visit, image, description, location }) => (!city || !country || !place || (!place_to_visit || !place_to_visit?.length) || !image || !description || !location || !location.lat || !location.long))
+        if (hasCorrectMode) return next()
+    }
+    res.status(500).json({ message: "Cities have not the correct model, every city should has city, country, place, place_to_visit, image, description, location(a json with lat and long property), all of these changes are required" })
+};
--- src/routes/cities.route.js
+++ src/routes/cities.route.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import controller from '../controllers/cities.controller.js';
+import { CitiesLoadNotHaveAllFields } from '../middlewares/cities.middleware.js';
 
 const router = express.Router();
 
 router.get('/', controller.getAllCities);
 router.get('/:id', controller.getCityById);
 router.post('/', controller.createCity);
+router.post('/load', CitiesLoadNotHaveAllFields, controller.createCities);
 router.put('/:id', controller.updateCity);
 router.delete('/:id', controller.deleteCity);
 
